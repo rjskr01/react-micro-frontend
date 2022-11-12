1616df511f43eac93d1a82cab16c11a7ee5d84b0
Rajasekar <rajasekar_gunasekaran@epam.com>
2022-11-12 15:31:28 +0530

    Removed unwanted dependencies

--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -1,14 +1,8 @@
-import { Box } from "@chakra-ui/react";
 import React from "react";
 import CounterAppOne from "./components/CounterAppOne";
 
 const App = () => (
-	<Box margin="1.2rem">
-		<Box>APP-1</Box>
-		<Box>
-			<CounterAppOne />
-		</Box>
-	</Box>
+	<CounterAppOne />
 );
 
 export default App;
